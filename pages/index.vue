8de9116923f0774e0269b1e612e7ea4e8c265fa3
mohamedhendy <mohamadhendy14@gmail.com>
2024-03-23 11:41:18 +0200

    inint site

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main">
+        <h1>hello  Eng  hendy</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    .main {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 90vh;
+    }
+
+</style>
